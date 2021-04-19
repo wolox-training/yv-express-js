7cfd201846bb6fac55168d8962427899d427a9c0
YAM-id <yamid.velez@wolox.co>
2021-04-19 08:32:35 -0500

    testing signup endpoint

--- migrations/migrations/20210414003111-create-user-table.js
+++ migrations/migrations/20210414003111-create-user-table.js
@@ -5,7 +5,7 @@ module.exports = {
     queryInterface.createTable('users', {
       id: { type: Sequelize.INTEGER, allowNull: false, unique: true, autoIncrement: true },
       name: { type: Sequelize.STRING, allowNull: false },
-      last_name: { type: Sequelize.STRING, allowNull: false },
+      lastName: { type: Sequelize.STRING, allowNull: false },
       mail: { type: Sequelize.STRING, allowNull: false, unique: true },
       password: { type: Sequelize.STRING, allowNull: false }
     }),
---
+++ test/users/index.test.js
@@ -0,0 +1,107 @@
+const supertest = require('supertest');
+
+const app = require('../../app');
+
+const {
+  mockUserSuccess,
+  mockUserMissingName,
+  mockUserMissingLastName,
+  mockUserMissingMail,
+  mockUserMissingPassword,
+  mockUserWrongMailDomain,
+  mockUserWrongPassword
+} = require('./_mocks');
+
+const request = supertest(app);
+
+describe('=== TESTING POST:/users endpoint ===', () => {
+  // TO DO => How to validate a type value
+  it('** Succesfull params', async done => {
+    await request
+      .post('/users')
+      .send(mockUserSuccess)
+      .then(response => {
+        expect(response.statusCode).toBe(200);
+        expect(response.text).toBe(`User [${mockUserSuccess.name}] has been created succesfully`);
+        done();
+      });
+  });
+
+  it('** Missing name', async done => {
+    await request
+      .post('/users')
+      .send(mockUserMissingName)
+      .then(response => {
+        expect(response.statusCode).toBe(400);
+        expect(response.body.errors[0].msg).toBe('Name is required');
+        done();
+      });
+  });
+
+  it('** Missing lastName', async done => {
+    await request
+      .post('/users')
+      .send(mockUserMissingLastName)
+      .then(response => {
+        expect(response.statusCode).toBe(400);
+        expect(response.body.errors[0].msg).toBe('Lastname is required');
+        done();
+      });
+  });
+
+  it('** Missing mail', async done => {
+    await request
+      .post('/users')
+      .send(mockUserMissingMail)
+      .then(response => {
+        expect(response.statusCode).toBe(400);
+        expect(response.body.errors[0].msg).toBe('Mail is required');
+        done();
+      });
+  });
+
+  it('** Exist user mail', async done => {
+    await request.post('/users').send(mockUserSuccess);
+    await request
+      .post('/users')
+      .send(mockUserSuccess)
+      .then(response => {
+        expect(response.statusCode).toBe(400);
+        expect(response.body.errors[0].msg).toBe('Email address already taken');
+        done();
+      });
+  });
+
+  it('** Wrong mail domain', async done => {
+    await request
+      .post('/users')
+      .send(mockUserWrongMailDomain)
+      .then(response => {
+        expect(response.statusCode).toBe(400);
+        expect(response.body.errors[0].msg).toBe('The mail domain is invalid');
+        done();
+      });
+  });
+
+  it('** Missing password', async done => {
+    await request
+      .post('/users')
+      .send(mockUserMissingPassword)
+      .then(response => {
+        expect(response.statusCode).toBe(400);
+        expect(response.body.errors[0].msg).toBe('Password is required');
+        done();
+      });
+  });
+
+  it('** Wrong password', async done => {
+    await request
+      .post('/users')
+      .send(mockUserWrongPassword)
+      .then(response => {
+        expect(response.statusCode).toBe(400);
+        expect(response.body.errors[0].msg).toBe('Password should be at least 8 chars long');
+        done();
+      });
+  });
+});
