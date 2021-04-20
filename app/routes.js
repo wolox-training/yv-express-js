234c16fc1f680703daa7e21dad4570f97f970098
YAM-id <yamid.velez@wolox.co>
2021-04-19 23:29:53 -0500

    modify user schema structure and add it as a middleware

--- app/routes.js
+++ app/routes.js
@@ -2,12 +2,11 @@
 const { healthCheck } = require('./controllers/healthCheck');
 
 const userController = require('./controllers/users');
-const { schemaValidate } = require('./middlewares/schemaValidate');
-const { signUpSchema } = require('./schemas/users');
+const { validateSignUpSchema } = require('./schemas/users');
 
 exports.init = app => {
   app.get('/health', healthCheck);
-  app.post('/users', signUpSchema(), schemaValidate, userController.signUp);
+  app.post('/users', [validateSignUpSchema], userController.signUp);
   // app.get('/endpoint/get/path', [], controller.methodGET);
   // app.put('/endpoint/put/path', [], controller.methodPUT);
   // app.post('/endpoint/post/path', [], controller.methodPOST);
