8e164bc8f0f507808f459fa166164476e32e3eef
YAM-id <yamid.velez@wolox.co>
2021-04-20 12:59:21 -0500

    fix test structure

--- test/users/index.test.js
+++ test/users/index.test.js
@@ -7,101 +7,140 @@ const {
   mockUserMissingName,
   mockUserMissingLastName,
   mockUserMissingMail,
-  mockUserMissingPassword,
   mockUserWrongMailDomain,
+  mockUserMissingPassword,
   mockUserWrongPassword
 } = require('./_mocks');
 
 const request = supertest(app);
+const postUserHelperRequest = mockData => request.post('/users').send(mockData);
 
 describe('=== TESTING POST:/users endpoint ===', () => {
   // TO DO => How to validate a type value
-  it('** Succesfull params', async done => {
-    await request
-      .post('/users')
-      .send(mockUserSuccess)
-      .then(response => {
-        expect(response.statusCode).toBe(201);
-        expect(response.text).toBe(`User [${mockUserSuccess.name}] has been created succesfully`);
-        done();
-      });
+  describe('** Succesfull params', () => {
+    let response = {};
+    beforeAll(async done => {
+      response = await postUserHelperRequest(mockUserSuccess);
+      done();
+    });
+    test('---> The status code must be "201" <---', () => {
+      expect(response.statusCode).toBe(201);
+    });
+    test(`---> The response text must be "User [${mockUserSuccess.name}] has been created succesfully" <---`, () => {
+      expect(response.text).toBe(`User [${mockUserSuccess.name}] has been created succesfully`);
+    });
   });
 
-  it('** Missing name', async done => {
-    await request
-      .post('/users')
-      .send(mockUserMissingName)
-      .then(response => {
-        expect(response.statusCode).toBe(400);
-        expect(response.body.errors[0].msg).toBe('Name is required');
-        done();
-      });
+  describe('** Missing name', () => {
+    let response = {};
+    beforeAll(async done => {
+      response = await postUserHelperRequest(mockUserMissingName);
+      done();
+    });
+
+    it('---> The status code must be "400" <---', () => {
+      expect(response.statusCode).toBe(400);
+    });
+
+    it('---> The response body error must be "Name is required" <---', () => {
+      expect(response.body.errors[0].msg).toBe('Name is required');
+    });
   });
 
-  it('** Missing lastName', async done => {
-    await request
-      .post('/users')
-      .send(mockUserMissingLastName)
-      .then(response => {
-        expect(response.statusCode).toBe(400);
-        expect(response.body.errors[0].msg).toBe('Lastname is required');
-        done();
-      });
+  describe('** Missing lastName', () => {
+    let response = {};
+    beforeAll(async done => {
+      response = await postUserHelperRequest(mockUserMissingLastName);
+      done();
+    });
+
+    it('---> The status code must be "400" <---', () => {
+      expect(response.statusCode).toBe(400);
+    });
+
+    it('---> The response body error must be "Lastname is required" <---', () => {
+      expect(response.body.errors[0].msg).toBe('Lastname is required');
+    });
   });
 
-  it('** Missing mail', async done => {
-    await request
-      .post('/users')
-      .send(mockUserMissingMail)
-      .then(response => {
-        expect(response.statusCode).toBe(400);
-        expect(response.body.errors[0].msg).toBe('Mail is required');
-        done();
-      });
+  describe('** Missing mail', () => {
+    let response = {};
+    beforeAll(async done => {
+      response = await postUserHelperRequest(mockUserMissingMail);
+      done();
+    });
+
+    it('---> The status code must be "400" <---', () => {
+      expect(response.statusCode).toBe(400);
+    });
+
+    it('---> The response body error must be "Mail is required" <---', () => {
+      expect(response.body.errors[0].msg).toBe('Mail is required');
+    });
   });
 
-  it('** Exist user mail', async done => {
-    await request.post('/users').send(mockUserSuccess);
-    await request
-      .post('/users')
-      .send(mockUserSuccess)
-      .then(response => {
-        expect(response.statusCode).toBe(400);
-        expect(response.body.errors[0].msg).toBe('Email address already taken');
-        done();
-      });
+  describe('** Exist user mail', () => {
+    let response = {};
+    beforeAll(async done => {
+      await postUserHelperRequest(mockUserSuccess);
+      response = await postUserHelperRequest(mockUserSuccess);
+      done();
+    });
+
+    it('---> The status code must be "400" <---', () => {
+      expect(response.statusCode).toBe(400);
+    });
+
+    it('---> The response body error must be "Email address already taken" <---', () => {
+      expect(response.body.errors[0].msg).toBe('Email address already taken');
+    });
   });
 
-  it('** Wrong mail domain', async done => {
-    await request
-      .post('/users')
-      .send(mockUserWrongMailDomain)
-      .then(response => {
-        expect(response.statusCode).toBe(400);
-        expect(response.body.errors[0].msg).toBe('The mail domain is invalid');
-        done();
-      });
+  describe('** Wrong mail domain', () => {
+    let response = {};
+    beforeAll(async done => {
+      response = await postUserHelperRequest(mockUserWrongMailDomain);
+      done();
+    });
+
+    it('---> The status code must be "400" <---', () => {
+      expect(response.statusCode).toBe(400);
+    });
+
+    it('---> The response body error must be "The mail domain is invalid" <---', () => {
+      expect(response.body.errors[0].msg).toBe('The mail domain is invalid');
+    });
   });
 
-  it('** Missing password', async done => {
-    await request
-      .post('/users')
-      .send(mockUserMissingPassword)
-      .then(response => {
-        expect(response.statusCode).toBe(400);
-        expect(response.body.errors[0].msg).toBe('Password is required');
-        done();
-      });
+  describe('** Missing password', () => {
+    let response = {};
+    beforeAll(async done => {
+      response = await postUserHelperRequest(mockUserMissingPassword);
+      done();
+    });
+
+    it('---> The status code must be "400" <---', () => {
+      expect(response.statusCode).toBe(400);
+    });
+
+    it('---> The response body error must be "Password is required" <---', () => {
+      expect(response.body.errors[0].msg).toBe('Password is required');
+    });
   });
 
-  it('** Wrong password', async done => {
-    await request
-      .post('/users')
-      .send(mockUserWrongPassword)
-      .then(response => {
-        expect(response.statusCode).toBe(400);
-        expect(response.body.errors[0].msg).toBe('Password should be at least 8 chars long');
-        done();
-      });
+  describe('** Wrong password', () => {
+    let response = {};
+    beforeAll(async done => {
+      response = await postUserHelperRequest(mockUserWrongPassword);
+      done();
+    });
+
+    it('---> The status code must be "400" <---', () => {
+      expect(response.statusCode).toBe(400);
+    });
+
+    it('---> The response body error must be "Password should be at least 8 chars long" <---', () => {
+      expect(response.body.errors[0].msg).toBe('Password should be at least 8 chars long');
+    });
   });
 });
