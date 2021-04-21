59ba668fe07d7969f48862ad22b9df4e9e1580bf
YAM-id <yamid.velez@wolox.co>
2021-04-20 21:48:29 -0500

    remove comments

--- app/routes.js
+++ app/routes.js
@@ -6,7 +6,4 @@ const { validateSignUpSchema } = require('./schemas/users');
 exports.init = app => {
   app.get('/health', healthCheck);
   app.post('/users', [validateSignUpSchema], userController.signUp);
-  // app.get('/endpoint/get/path', [], controller.methodGET);
-  // app.put('/endpoint/put/path', [], controller.methodPUT);
-  // app.post('/endpoint/post/path', [], controller.methodPOST);
 };
